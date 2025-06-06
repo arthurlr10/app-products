e850f3d02de3ec98fc780d52b4a253437c71afde
Arthur LORY <alory@zaka-services.com>
2025-06-06 09:16:54 +0200

    fix step-docker

--- php/www/connect.php
+++ php/www/connect.php
@@ -1,11 +1,16 @@
 <?php
-    $host = "db";
+    /* $host = "db";
     $username = "root";
     $password = "root";
-    $db = "gestion_produits";
+    $db = "gestion_produits"; */
+
+    $host = getenv('DB_HOST') ?: 'mysql';
+    $dbName = getenv('DB_NAME') ?: 'gestion_produits';
+    $user = getenv('DB_USER') ?: 'root';
+    $password = getenv('DB_PASS') ?: 'root';
 
     // Connexion avec pdo mysql
-    $db = new PDO("mysql:host=$host;dbname=$db", $username, $password);
+    $db = new PDO("mysql:host=$host;dbname=$dbName", $user, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
